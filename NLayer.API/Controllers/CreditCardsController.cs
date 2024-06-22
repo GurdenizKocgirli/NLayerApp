728fd6115dd5be84a4e518e80d3c0fe53945579c
Gürdeniz Kocgirli <gurdenizkocgirli_@outlook.com>
2024-06-22 19:08:29 +0300

    Comments added to the CreditCardsController and ProductsController

--- NLayer.API/Controllers/CreditCardsController.cs
+++ NLayer.API/Controllers/CreditCardsController.cs
@@ -22,50 +22,50 @@ namespace NLayer.API.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> GetCreditCardsWithUsers()
         {
-            return CreateActionResult(await _creditCardService.GetCreditCardsWithUsers());
+            return CreateActionResult(await _creditCardService.GetCreditCardsWithUsers()); //CreateActionResult metodu GetCreditCardWithUsers metodundan dönen sonucu alır ve bunu IActionResult türüne dönüştürerek HTTP yanıtı oluşturur.
         }
 
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var creditCards = await _creditCardService.GetAllAsync();
-            var creditCardDtos = _mapper.Map<List<CreditCardDto>>(creditCards.ToList());
-            return CreateActionResult(CustomResponseDto<List<CreditCardDto>>.Success(200, creditCardDtos));
+            var creditCards = await _creditCardService.GetAllAsync(); //_creditCardService, kredi kartıyla ilgili business işlemleri için tanımlanmıştır ve asenkron olarak kredi kartı bilgilerini getirir ve creditCards değişkenine atar
+            var creditCardDtos = _mapper.Map<List<CreditCardDto>>(creditCards.ToList()); //creditCards.ToList metoduyla creditCards listesi List<CreditCard> listesine dönüştürülür ve bu liste _mapper ile List<CreditCardDto> türüne dönüştürülür ve creditCardDtos değişkenine atılır.
+            return CreateActionResult(CustomResponseDto<List<CreditCardDto>>.Success(200, creditCardDtos)); //CustomResponseDto özel bir yanıt DTO'su ve bu DTO, List<CreditCardDto> türünde bir veri içerir. 200 başarılı http durum kodudur ve bu durum kodunun içeriği creditCardDtos değişkenidir. CreateActionResult metodu ile birlikte bu CustomResponseDto bir http yanıtı olarak döndürülür.
         }
 
-        [ServiceFilter(typeof(NotFoundFilter<CreditCard>))]
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
+        [ServiceFilter(typeof(NotFoundFilter<CreditCard>))] //Eğer belirli bir id'ye sahip olan CreditCard nesnesi bulunamazsa NotFoundFilter<CreditCard> otomatik olarak bir hata yanıtı dönecektir
+        [HttpGet("{id}")] //Belirli bir id'ye sahip olan nesne için http get isteğidir
+        public async Task<IActionResult> GetById(int id) //asenkron olarak çalışan bu GetById metodu belirli bir id'ye göre Task<IActionResult> türünde bir yanıt döndürür
         {
-            var creditCards = await _creditCardService.GetByIdAsync(id);
-            var creditCardDtos = _mapper.Map<CreditCardDto>(creditCards);
-            return CreateActionResult(CustomResponseDto<CreditCardDto>.Success(200, creditCardDtos));
+            var creditCards = await _creditCardService.GetByIdAsync(id); //_creditCardService, kredi kartıyla ilgili işlemlerdir ve belirli bir id'ye göre asenkron olarak getirilen kredi kartı bilgisini creditCards değişkenine atar
+            var creditCardDtos = _mapper.Map<CreditCardDto>(creditCards); //creditCards nesnesindeki değişken _mapper aracılığıyla CreditCardDto veri transfer nesnesine dönüştürülür ve bu nesne creditCardDtos nesnesine atanır
+            return CreateActionResult(CustomResponseDto<CreditCardDto>.Success(200, creditCardDtos)); //CreditCardDto için özel bir yanıt olan CustomResponseDto 
         }
 
         [HttpPost]
-        public async Task<IActionResult> Save(CreditCardDto creditCardDto)
+        public async Task<IActionResult> Save(CreditCardDto creditCardDto) //CreditCardDto nesnesi http post isteğiyle alınır bu nesne kredi kartı bilgilerini içerir
         {
-            var creditCards = await _creditCardService.AddAsync(_mapper.Map<CreditCard>(creditCardDto));
-            var creditCardDtos = _mapper.Map<CreditCardDto>(creditCards);
-            return CreateActionResult(CustomResponseDto<CreditCardDto>.Success(201, creditCardDtos));
+            var creditCards = await _creditCardService.AddAsync(_mapper.Map<CreditCard>(creditCardDto)); //kredi kartı bilgilerini içeren creditCardDto nesnesi CreditCard türüne dönüştürülür(mapping) ardından bu değer veri tabanına eklenir
+            var creditCardDtos = _mapper.Map<CreditCardDto>(creditCards); //creditcards değişkenindeki kredi kartı bilgileri CreditCardDto nesnesine dönüştürülür ve creditCArdDtos değişkenine atanır
+            return CreateActionResult(CustomResponseDto<CreditCardDto>.Success(201, creditCardDtos)); //CreateActionResult ile CustomResponseDto özel yanıtı döndürülür
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(CreditCardUpdateDto creditCardDto)
+        public async Task<IActionResult> Update(CreditCardUpdateDto creditCardDto) //Update edilecek credit card bilgileri CreditCardUpdateDto ile creditCardDto üzerinden alınır
         {
-            await _creditCardService.UpdateAsync(_mapper.Map<CreditCard>(creditCardDto));
+            await _creditCardService.UpdateAsync(_mapper.Map<CreditCard>(creditCardDto)); //creditCardDto veri transfer nesnesi ile alınan kredi kartı bilgileri _mapper ile CreditCard veri modeline dönüştürülür ve dönüştürülen CreditCard asenkron bir şekilde güncellenir
 
-            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204)); //Güncellenen kredi kartı bilgileri CustomResponseDto özel yanıt veri transferi ile NoContentDto türünde geriye bir şey dönmeyecek şekilde CreateActionResult yardımcı metodu ile IActionResult tipinde client'a döndürülür
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Remove(int id)
+        [HttpDelete("{id}")] //client tarafından silme işlemi isteği bu controller'a düşer ve bunu yönlendiren de route'dur. belirli bir veri id'sine göre olduğu dikkatimizden kaçmamalı
+        public async Task<IActionResult> Remove(int id) //silinecek olan kredi kartının id si HttpDelete isteği ile alınır
         {
-            var creditCard = await _creditCardService.GetByIdAsync(id);
+            var creditCard = await _creditCardService.GetByIdAsync(id); //id'si alınan kredi kartının _creditCardService.GetByIdAsync(id) ile asenkron olarak getirilmesi ve creditCard değişkenine atanması sağlanır
 
-            await _creditCardService.RemoveAsync(creditCard);
+            await _creditCardService.RemoveAsync(creditCard); //kredi kartı işlemlerinin yapıldığı _creditCardService ile bir önceki satırda id'si alınan kredi kartının veri tabanından silme işlemi tamamlanır ve asenkron olarak bu işlem yapılır
 
-            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204)); //NoContent olacak şekilde 204 durum kodlu http yanıtı client'a döndürülür.
         }
     }
 }
--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -7,31 +7,31 @@ using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
 {
-    public class ProductsController : CustomBaseController
+    public class ProductsController : CustomBaseController //CustomBaseController sınıfı base bir class olduğu için içerisinde zaten Route mekanizması vardır bu yüzden ProductsController class'ında tekrardan bir route tanımlamamıza gerek yoktur. Route gelen isteklerin hangi Controller sınıfına yönlendireceğine karar veren bir özelliktir
     {
-        private readonly IMapper _mapper;
-        private readonly IProductService _service;
+        private readonly IMapper _mapper; //Mapping işlemlerinin yapılabilmesi için IMapper türünde bağımlı bir _mapper değişkeninin oluşturulması
+        private readonly IProductService _service; //Product ile ilgili business işlemlerinin yapılabilmesi için IProductService türünde bir _service değişkeninin tanımlanması
 
-        public ProductsController(IMapper mapper, IProductService productService)
+        public ProductsController(IMapper mapper, IProductService productService) //Bağımlılıkların azaltılması için IMapper türünde bir mapper parametresi ve IProductService türünde bir productService parametresi tanımladık ve bağımlılığı dışarıdan vermiş olduk
         {
             _mapper = mapper;
             _service = productService;
         }
 
-        /// GET api/products/GetProductsWithCategory
-        [HttpGet("[action]")]
-        public async Task<IActionResult> GetProductsWithCategory()
-        {
-            return CreateActionResult(await _service.GetProductsWithCategory());
+        /// GET api/products/GetProductsWithCategory --> bu kısım bizim endpoint'imizdir yani client'ın istek attığı url aslında budur. endpointlerin temiz bir şekilde kodlanması da best practice açısından önemlidir
+        [HttpGet("[action]")] //Client bana Kategorilerle birlikte bütün ürünleri göster isteği atar ve action metoduna düşer
+        public async Task<IActionResult> GetProductsWithCategory() //await _service.GetProductsWithCategory metodu asenkron olarak ürünlerle birlikte kategorilerini getirir
+        {//projemizde asenkron işlemler yaptık çünkü bir işlemin tamamlanmasını beklerken aynı zamanda diğer işlemlerin de yapılmasını sağlamak performansı arttırır burada da async ile bu metodun asenkron olarak çalışacağını belirtmiş oluyoruz
+            return CreateActionResult(await _service.GetProductsWithCategory()); //CreateActionResult ile getirilen ürünler isteği atan client'a düzenlenip http yanıtı ile gösterilir ve await ile birlikte bu işlemin yapılması beklenirken diğer işlemlerin yapılabilmesi sağlanır
         }
 
         /// GET api/products
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var products = await _service.GetAllAsync();
-            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
-            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+            var products = await _service.GetAllAsync(); //await asenkron metodun tamamlanmasını bekler ve sonucu products değişkenine atar
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList()); // Map<List<ProductDto>>(products.ToList()) products listesindeki her bir ürünü ProductDto nesnesine dönüştürerek productsDtos listesine atar
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos)); //CreateActionResult, CustomResponseDto nesnesini alır ve bir IActionResult döndürür ve bu return ile HTTP istemcisine yanıt olarak gönderilir
         }
 
     [ServiceFilter(typeof(NotFoundFilter<Product>))]
