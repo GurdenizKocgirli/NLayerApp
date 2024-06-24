64e5beb0f53e6f1392849d3b784b2a70a7b25c57
GMK <gurdenizkocgirli_@outlook.com>
2024-06-24 14:24:25 +0300

    comments added to UsersController

--- NLayer.API/Controllers/UsersController.cs
+++ NLayer.API/Controllers/UsersController.cs
@@ -2,33 +2,31 @@
 using Microsoft.AspNetCore.Mvc;
 using NLayer.Core.DTOs;
 using NLayer.Core.Services;
-using NLayer.Service.Services;
-using System.Collections.Generic;
 
 namespace NLayer.API.Controllers
 {
-    public class UsersController : CustomBaseController
+    public class UsersController : CustomBaseController //user ile ilgili işlemler yapılacağı zaman client tarafından gelen isteği route buraya yönlendirir
     {
         private readonly IUserService _userService;
         private readonly IMapper _mapper;
 
-        public UsersController(IUserService userService, IMapper mapper)
+        public UsersController(IUserService userService, IMapper mapper) //bağımlılıkların gevşetilmesi için dışarıdan enjekte edilmesi
         {
-            _userService = userService;
+            _userService = userService; 
             _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll() //bütün userların getirilmesi istenildiğinde çalışacak olan metod
         {
-            var users = await _userService.GetAllAsync();
-            var usersDto = _mapper.Map<List<UserDto>>(users.ToList());
+            var users = await _userService.GetAllAsync(); //asenkron olarak db'den bütün userların getirilip users değişkenine atanması
+            var usersDto = _mapper.Map<List<UserDto>>(users.ToList()); //getirilmiş olan user listesi UserDto listesine dönüştürülür ve usersDto değişkenine atanır
 
-            return CreateActionResult(CustomResponseDto<List<UserDto>>.Success(200, usersDto));
+            return CreateActionResult(CustomResponseDto<List<UserDto>>.Success(200, usersDto)); //UserDto listesi özel bir yanıt sisteöiyle CreateActionResult yardımcı metoduyla IActionResult türünde döndürülür ve http yanıtı olarak client'a gösterilir
         }
 
         [HttpGet("[action]/{userId}")]
-        public async Task<IActionResult> GetSingleUserByIdWithCreditCardsAndUsersAsync(int userId)
+        public async Task<IActionResult> GetSingleUserByIdWithCreditCardsAndUsersAsync(int userId) //userlar ile birlikte kredi kartlarının getirilmesini sağlayan metod
         {
             return CreateActionResult(await _userService.GetSingleUserByIdWithCreditCardsAndUsersAsync(userId));
         }
