13b8f596589372c61ed67371ec421a112f8de26b
GMK <gurdenizkocgirli_@outlook.com>
2024-06-24 14:19:56 +0300

    comments added to CategoriesController class

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -5,33 +5,33 @@ using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
 {
-    public class CategoriesController : CustomBaseController
+    public class CategoriesController : CustomBaseController //kategoriler ile ilgili controller işlemlerinin yapıldığı class
     {
-        private readonly ICategoryService _categoryService;
-        private readonly IMapper _mapper;
-        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        private readonly ICategoryService _categoryService; //kategori servis işlemlerinin yapıldığı ve atandığı değişken
+        private readonly IMapper _mapper; // mapping işlemleri için tanımlanmış olan değişken
+        public CategoriesController(ICategoryService categoryService, IMapper mapper) //private olarak tanımlanmış olan _categoryService ve _mapper değişkenlerine bağımlılıkların yapıcı metod içerisinde dışarıdan verilmesi
         {
             _categoryService = categoryService;
             _mapper = mapper;
         }
 
-        [HttpGet]
+        [HttpGet] //Bütün kategorilerin getirilmesi istendiği zaman aşağıdaki metod çalışır
         public async Task<IActionResult> GetAll()
         {
 
-            var categories = await _categoryService.GetAllAsync();
+            var categories = await _categoryService.GetAllAsync(); //bütün kategoriler asenkron olarak db'den çekildikten sonra categories değişkenine atanır
 
-            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList()); //çekilmiş olan List<Category> listesi _mapper ile List<CategoryDto>'ya dönüştürülür ve elde edilen sonuç categoriesDto değişkenine atanır
 
-            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200,categoriesDto));
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200,categoriesDto)); //CategoryDto türündeki liste özel bir yanıt dto'su olan CustomResponseDto ile CreateActionResult yardımıyla IActionResult türünde döndürülür
         
         }
 
         // api/categories/GetSingleCategoryByIdWithProducts/2
-        [HttpGet("[action]/{categoryId}")]
-        public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
+        [HttpGet("[action]/{categoryId}")] //Belirli bir kategorinin sahip olduğu ürünleri görmek için kullanılan http isteği
+        public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId) 
         {
-            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId)); //client tarafından gönderilen kategori id'si _categoryService ile birlikte asenkron olarak db'den çekilir ve CreateActionResult yardımcı metoduyla IActionResult türünde http yanıtı olarak client'a gösterilir
         }
     }
 }
