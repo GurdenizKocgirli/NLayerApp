51b0285e2ea7354ceaa3ea18480274036d54a86a
GMK <gurdenizkocgirli_@outlook.com>
2024-06-26 15:13:37 +0300

    RepoServiceModul comments has been added

--- NLayer.API/Modules/RepoServiceModule.cs
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -11,26 +11,26 @@ using System.Reflection;
 using Module = Autofac.Module;
 namespace NLayer.API.Modules
 {
-    public class RepoServiceModule:Module
-    {
+    public class RepoServiceModule:Module //AutoFac kütüphanesine ait olan Module sınıfının miras alınması. Bağımlılıkların yönetilmesi için kullanılan bir kütüphanedir
+    {//AutoFac bir IOC konteyneri'dir ve bağımlılıkları yönetmeye yarar
 
-        protected override void Load(ContainerBuilder builder)
+        protected override void Load(ContainerBuilder builder) //AutoFac'in Module sınıfından gelen bir metod olan Load, dependency yapılandırması için kullanılır
         {
+            //builder sınıfı dependency'leri kaydetmek için kullanılır. Load metodu içerisinde builder nesnesi üzerinden kayıtlar yapılarak Autofac konteynerine eklenir
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope(); //GenericRepository'nin IGenericRepository'ye denk geldiği belirtilir ve InstancePerLifeTimeScope ile döngünün yaşam süresi belirlenir
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope(); //Service, IService'e denk gelir
 
-            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
-            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
-
-            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
-
-
-            var apiAssembly = Assembly.GetExecutingAssembly();
-            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
-            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
-
-            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>(); //UnitOfWork sınıfı IUnitOfWork interface'ine karşılık gelir ve bu şekilde kaydedilir
 
 
-            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            var apiAssembly = Assembly.GetExecutingAssembly(); //API projesinin assembly'si alınır
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext)); //repository projesinin assembly'si AppDbContext sınıfından alınır
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile)); //service projesinin assembly'si MapProfile sınıfından alınır
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope(); //repository sınıfları IRepository interface'ine denk gelir ve bu şekilde kaydedilir
+
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope(); //Service sınıfları IService interface'ine denk gelir ve bu şekilde kaydedilir
 
 
            // builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
